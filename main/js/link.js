5c8cdeb0513232dbdcb1ca6b49300ba1d70361e5
matic <msc0101312@gmail.com>
2025-02-09 23:45:21 +0900

    2025 02 09 / header, footer 모듈화 완료

--- main/js/link.js
+++ main/js/link.js
@@ -1,1 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("../header/header.html") // header.html 파일 가져오기
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("headerContainer").innerHTML = data;
 
+            let link = document.createElement("link");
+            link.rel = "stylesheet";
+            link.href = "../header/css/header.css";
+            document.head.appendChild(link);
+
+      })
+      .catch(error => console.error("헤더 로딩 실패:", error));
+
+      fetch("../footer/footer.html") // footer.html 파일 가져오기
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("footerContainer").innerHTML = data;
+
+            let link = document.createElement("link");
+            link.rel = "stylesheet";
+            link.href = "../footer/css/footer.css";
+            document.head.appendChild(link);
+
+      })
+      .catch(error => console.error("헤더 로딩 실패:", error));
+  });  
